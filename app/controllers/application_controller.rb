eb2343f66e4bd30555178f4c71510dc71c61ce97
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-17 17:05:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,15 @@ class App < Sinatra::Base
     post '/team' do
       @team_name = params[:team][:name]
       @team_motto = params[:team][:motto]
-      @hero_name= params[:team][:members][0][:name]
-      @hero_power= params[:team][:members][0][:power]
-      @hero_bio = params[:team][:members][0][:bio]
+      @hero1_name= params[:team][:members][0][:name]
+      @hero1_power= params[:team][:members][0][:power]
+      @hero1_bio = params[:team][:members][0][:bio]
+      @hero2_name=params[:team][:members][1][:name]
+      @hero2_power= params[:team][:members][1][:power]
+      @hero2_bio= params[:team][:members][1][:bio]
+      @hero3_name= params[:team][:members][2][:name]
+      @hero3_power= params[:team][:members][2][:power]
+      @hero3_bio= params[:team][:members][2][:bio]
 
 
 
